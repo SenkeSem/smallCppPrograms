f17bafdeb7fc98e84edbce590ec4e742182ff42f
SenkeSem <sen9morf@gmail.com>
2025-01-07 13:21:05 +0300

    jyt app

---
+++ jupiterYearsTranslator.cpp
@@ -0,0 +1,18 @@
+// Program for convertation Jupiter years in Earth years
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	double jupiterYears;
+	double earthYears;
+
+	cout << "Enter the number of years on Jupiter: ";
+	cin >> jupiterYears;
+
+	earthYears = jupiterYears / 12;
+	cout << "Number of Earth years: " << earthYears;
+
+	return 0;
+}
