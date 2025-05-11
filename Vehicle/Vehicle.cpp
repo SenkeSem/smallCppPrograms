b74f0de567a117d070438617e0308ecb646ac8be
SenkeSem <sen9morf@gmail.com>
2025-05-11 17:05:45 +0300

    V 1.1

--- Vehicle/Vehicle.cpp
+++ Vehicle/Vehicle.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
 
 class Vehicle {
-public:
 	int passengers;
 	int fuelcap;
 	int mpg;
+public:
+	Vehicle(int p, int f, int m) {
+		passengers = p;
+		fuelcap = f;
+		mpg = m;
+	}
+	
+	int range() { return fuelcap * mpg; }
+	int get_passengers() { return passengers; }
+	int get_fuelcap() { return fuelcap; }
+	int get_mpg() { return mpg; }
+};
+
+class Truck : public Vehicle {
+	int cargocap;
+public:
+	Truck(int p, int f, int m, int c) : Vehicle(p, f, m) {
+		cargocap = c;
+	}
+	
+	int get_cargocap() { return cargocap; }
 };
 
 int main()
 {
-	Vehicle minivan;
-	int range;
+	Truck semi(2, 200, 7, 44000);
+	Truck pickup(3, 28, 15, 2000);
+	int dist = 252;
 	
-	minivan.passengers = 7;
-	minivan.fuelcap = 16;
-	minivan.mpg = 21;
+	std::cout << "Semi data:" << std::endl;
+	std::cout << "Can carry " << semi.get_cargocap() << " pounds of cargo" << std::endl;
+	std::cout << "Mileage range: " << semi.range() << std::endl;
+	std::cout << "A " << dist << "-mile journey requires "
+	<< dist / semi.get_mpg() << " gallons of fuel"
+	<< std::endl;
+	std::cout << "\n";
 	
-	range = minivan.fuelcap * minivan.mpg;
-	
-	std::cout << "The van can drive "
-	<< minivan.passengers
-	<< " passengers over a distance of "
-	<< range
-	<< " miles" << std::endl;
+	std::cout << "pickup data:" << std::endl;
+	std::cout << "Can carry " << pickup.get_cargocap() << " pounds of cargo" << std::endl;
+	std::cout << "Mileage range: " << pickup.range() << std::endl;
+	std::cout << "A " << dist << "-mile journey requires "
+	<< dist / pickup.get_mpg() << " gallons of fuel"
+	<< std::endl;
+	std::cout << "\n";
 	
 	return 0;
 }
